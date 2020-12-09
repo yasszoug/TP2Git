f34c8e8fe84450a60fe75f38614a3289d121be63
yassine <ysnowtech@gmail.com>
2020-12-09 10:52:55 +0100

    commit 1 - TP2 - GL

--- app/src/main/java/com/example/android/tp2_git/MainActivity.java
+++ app/src/main/java/com/example/android/tp2_git/MainActivity.java
@@ -11,4 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void MethodAleatoireGit(){
+        System.out.println("Just something");
+    }
 }
